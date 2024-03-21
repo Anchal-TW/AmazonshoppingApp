2ed9bc0c96926ece83a75dec12c033e52b20bbf2
Jaswantth Potluri <jaswantthpotluri@Jaswantths-MacBook-Pro.local>
2024-03-21 21:25:38 +0530

    Refactor. home page styles

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -36,9 +36,12 @@ const Home = () => {
       <View style={styles.itemContainer}>
         <Image style={styles.image} source={{uri: item.image}} />
         <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.price}>{item.price}</Text>
+        <View style={styles.priceContainer}>
+          <Text style={styles.dollar}>$</Text>
+          <Text style={styles.price}>{item.price}</Text>
+        </View>
         <Text style={styles.rating}>
-          Rating: {item.rating.rate} ({item.rating.count} reviews)
+          Rating: {item.rating.rate} 
         </Text>
       </View>
     );
@@ -61,117 +64,53 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'whitesmoke',
-    padding: 10, // Adjust background color as needed
   },
   itemContainer: {
     flex: 1,
     padding: 10,
     margin: 10,
-    borderRadius: 20,
-    backgroundColor: 'white', // Adjust background color as needed
+    borderRadius: 30,
+    backgroundColor: 'white',
   },
   image: {
     width: '100%',
-    height: 160,
+    height: 150,
     resizeMode: 'cover',
     borderRadius: 8,
   },
   title: {
-    fontSize: 16,
+    flex:1,
+    fontSize: 15,
     fontWeight: 'bold',
     marginTop: 8,
+    color: 'lightblue',
+    padding: 10,
+    textAlign:'center'
   },
   price: {
-    fontSize: 14,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'darkblue',
+    textAlign: 'center',
+  },
+  priceContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
     marginTop: 4,
+    justifyContent:'center'
+  },
+  dollar: {
+    fontSize: 12,
+    color: 'darkblue',
+    marginTop: 2,
+    marginRight: 2,
   },
   rating: {
     fontSize: 14,
     marginTop: 4,
+    textAlign:'center'
   },
 });
 
 export default Home;
 
-// import React, {useEffect, useState} from 'react';
-// import {Image, ScrollView, Text, View, FlatList} from 'react-native';
-// import {useTheme} from '../store/ThemeProvider-Context';
-
-// interface User {
-//   id: number;
-//   title: string;
-//   price: string;
-//   category: string;
-//   description: string;
-//   image: string;
-// }
-// const Home = () => {
-//   const {backgroundColor, textColor} = useTheme();
-//   const [items, setItems] = useState<User[]>([]);
-
-//   const getListItem = async () => {
-//     const url = 'https://fakestoreapi.com/products';
-//     fetch(url)
-//       .then(resp => resp.json())
-//       .then(json => {
-//         setItems(json);
-//       })
-//       .catch(error => console.error(error));
-//   };
-
-//   useEffect(() => {
-//     getListItem();
-//   }, []);
-
-//   const displayItem = (itemData : any) => {
-//     return (
-//             <View style={{flex:1,padding: 16}}>
-//               {/* <Text style={{backgroundColor: 'blanchedalmond'}}>
-//                 id: {itemData.id}
-//               </Text> */}
-//               <Text>title: {itemData.title}</Text>
-//               <Text>price: {itemData.price}</Text>
-//               <Image
-//                 style={{
-//                   width: 51,
-//                   height: 51,
-//                   resizeMode: 'contain',
-//                 }}
-//                 source={{uri: itemData.image}}></Image>
-//             </View>
-//           );
-//   }
-
-//   return (
-//     <FlatList
-//       data={items}
-//       keyExtractor={(item) => String(item.id)}
-//       renderItem={displayItem}
-//       numColumns={2}/>
-//     // <ScrollView
-//     //   style={{
-//     //     backgroundColor,
-//     //   }}>
-//     //   {items.map(item => {
-//     //     return (
-//     //       <View style={{padding: 16}}>
-//     //         <Text style={{backgroundColor: 'blanchedalmond'}}>
-//     //           id: {item.id}
-//     //         </Text>
-//     //         <Text>title: {item.title}</Text>
-//     //         <Text>price: {item.price}</Text>
-//     //         <Image
-//     //           style={{
-//     //             width: 51,
-//     //             height: 51,
-//     //             resizeMode: 'contain',
-//     //           }}
-//     //           source={{uri: item.image}}></Image>
-//     //       </View>
-//     //     );
-//     //   })}
-//     // </ScrollView>
-//   );
-// };
-
-// export default Home;
